a68dd4391b9be8549c21e20d9cbe3f6772086a5b
Vinicius Siqueira <vinicius.siqueira642@gmail.com>
2023-06-08 19:46:03 -0300

    feat: adição de uma rota get na raiz do projeto

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,5 @@
 import cors from 'cors'
-import { Router } from "express"
+import { Request, Response, Router } from "express"
 
 import { decryptionRouter } from "./decryption.routes"
 import { encryptionRouter } from "./encryption.routes"
@@ -10,6 +10,10 @@ export const router = Router()
 router.use(cors({
     origin: 'http://localhost:3000'
 }))
+
+router.get('/', (_request: Request, response: Response) => {
+    return response.send('Express Typescript on Vercel')
+})
 
 router.use("/encryption", encryptionRouter)
 router.use("/decryption", decryptionRouter)
