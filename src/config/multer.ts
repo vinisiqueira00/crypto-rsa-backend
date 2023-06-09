7af672ffb40ec38e079634f53f6fa3be5398008a
Vinicius Siqueira <vinicius.siqueira642@gmail.com>
2023-06-08 22:11:35 -0300

    feat: corrige caminho de armazenamento dos arquivos

--- src/config/multer.ts
+++ src/config/multer.ts
@@ -4,7 +4,7 @@ import path from "path";
 export const multerConfig = {
     storage: multer.diskStorage({
         destination: (req: any, file: any, cb: any) => {
-            cb(null, path.resolve(__dirname, '..', '..', 'tmp', 'uploads'))
+            cb(null, path.resolve(__dirname, '..', '..', 'tmp'))
         },
         filename: (req: any, file: any, cb: any) => {
             const ext = path.extname(file.originalname)
